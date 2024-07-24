6556793d8cd1561e6b38dbe4fb4c98b70c7273dd
gcros <guilhem.cros.38@gmail.com>
2024-07-25 00:17:27 +0200

    mod3 ex03

---
+++ mod03/ex03/DiamondTrap.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 22:27:33 by gcros             #+#    #+#             */
+/*   Updated: 2024/07/24 23:41:03 by gcros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "DiamondTrap.hpp"
+
+	/*	constructor	*/
+DiamondTrap::DiamondTrap():
+ScavTrap(), FragTrap()
+{
+	std::cout << "[DiamondTrap]	Default constructor called" << std::endl;
+	this->name = ClapTrap::name;
+	ClapTrap::name = ClapTrap::name + "_clap_name";
+	hitPoint = 100;
+	energy = 100;
+	damage = 30;
+}
+
+DiamondTrap::DiamondTrap(std::string name):
+ScavTrap(name), FragTrap(name)
+{
+	std::cout << "[DiamondTrap]	name constructor called" << std::endl;
+	this->name = ClapTrap::name;
+	ClapTrap::name = ClapTrap::name + "_clap_name";
+	hitPoint = 100;
+	energy = 100;
+	damage = 30;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &value):
+ClapTrap(value), ScavTrap(value), FragTrap(value)
+{
+	std::cout << "[DiamondTrap]	Copy constructor called" << std::endl;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+	std::cout << "[DiamondTrap]	Destructor called" << std::endl;
+}
+
+	/*	utils	*/
+DiamondTrap	&DiamondTrap::operator=(const DiamondTrap &value)
+{
+	this->damage = value.damage;
+	this->name = value.name;
+	this->hitPoint = value.hitPoint;
+	this->energy = value.energy;
+	return (*this);
+}
+
+void	DiamondTrap::whoAmI()
+{
+	std::cout << "[DiamondTrap]	I am :" << this->name << ", as: " << this->ClapTrap::getName() << std::endl;
+}
---
+++ mod03/ex03/DiamondTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 22:27:35 by gcros             #+#    #+#             */
+/*   Updated: 2024/07/24 23:38:06 by gcros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _DIAMONDTRAP_HPP_
+#define _DIAMONDTRAP_HPP_
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap: public ScavTrap, public FragTrap
+{
+
+	public:
+	/*	constructor	*/
+		DiamondTrap();
+		DiamondTrap(std::string name);
+		DiamondTrap(const DiamondTrap &value);
+		~DiamondTrap();
+
+	/*	utils	*/
+		DiamondTrap	&operator=(const DiamondTrap &value);
+		void		whoAmI();
+
+	private:
+		std::string	name;
+};
+
+#endif // _DIAMONDTRAP_HPP_
